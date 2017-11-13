e55a90c61e6f184da99dd6fa462a2243dc6579f1
Gabriele Valastro <33549583+valagab@users.noreply.github.com>
2017-11-13 23:58:53 +0100

    Add files via upload

---
+++ exercise_medium.py
@@ -0,0 +1,11 @@
+# 2) Crea una sottoclasse MyPet che eredita da Pet e aggiunge l'attributo 'owner' come stringa 
+#       e il metodo 'say_hello' che torna 'meow' se il type è cat, 'bau' se è dog e 'hihihi' se è horse.
+
+
+# Creare una repository e per ogni esercizio fare un commit in una branch separata e fare il merge su master alla fine di ogni esercizio.
+# Ogni classe deve stare su un file separato.
+
+# Voti:
+# - Bah: non ne fai neanche uno
+# - Ci siamo quasi: ne fai due
+# - Boss: li fai tutti e 3
